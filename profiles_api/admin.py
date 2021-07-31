520a7e85a5add3b0ab16519df1f20051246767f0
kdhruvajyothreddy <kdhruvajyothreddy@gmail.com>
2021-07-31 21:59:57 +0530

    Register User Profile model in admin file

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from profiles_api import models
+
 
 # Register your models here.
+# To register User Profile model with Admin site
+admin.site.register(models.UserProfile)
