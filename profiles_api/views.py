d51715fcee7c9b7d45901b44c724a8c1e4f3373f
kdhruvajyothreddy <kdhruvajyothreddy@gmail.com>
2021-08-01 17:14:58 +0530

    Adding API View and setting up the urls to access response on the browser

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 # Create your views here.
+class HelloAPIView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of API View features"""
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your '
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
